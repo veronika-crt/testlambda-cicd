4c8ada6531fc7780a54b0f66fbc990eac1631c0d
veronika-crt <veronika@d-sat.com>
2025-08-21 10:39:03 +0530

    Lambda function

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,3 +3,13 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": "Hello from Lambda!"
     }
+
+
+import json
+
+def greet_handler(event, context):
+    name = event.get("name", "Guest")
+    return {
+        "statusCode": 200,
+        "body": json.dumps(f"Hello {name}, welcome to AWS Lambda!")
+    }
